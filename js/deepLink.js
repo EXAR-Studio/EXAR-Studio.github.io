c3a0d0ae5ae3ae8b400e328df872fb452a94eac2
Kyle Giammarco <kyle@giammar.co>
2024-12-09 22:49:48 -0500

    Add deep linking functionality to Cultured App
    Replaced 'open' link with JavaScript-based deep linking to improve app integration. Added deepLink.js script that constructs a custom URL based on query parameters. Removed duplicate index.html file to streamline project structure.

--- js/deepLink.js
+++ js/deepLink.js
@@ -2,7 +2,5 @@ window.onload = function () {
     var URL = window.location.href;
 
     var params = new URLSearchParams(new URL(URL).search);
-    var newUrl = 'cltrd://open?' + params.toString();
-
-    document.getElementById('openApp').href = newUrl;
+    document.getElementById('openApp').href = 'cltrd://open?' + params.toString();
 };
